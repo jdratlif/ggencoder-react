552a8e379d8074fde54d4fba2db6e907c6642d75
John Ratliff <john@technoplaza.net>
2021-11-21 17:20:42 -0500

    add Footer component

--- ggencoder/src/App.tsx
+++ ggencoder/src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header version="0.0.1" />
+      <Footer />
     </>
   );
 }
---
+++ ggencoder/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h5>
+        Copyright (c) 2004-2006, 2008, 2016, 2021
+        <a href="http://games.technoplaza.net">emuWorks</a>
+      </h5>
+    </footer>
+  );
+};
+
+export default Footer;
